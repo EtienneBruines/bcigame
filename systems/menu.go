2c35b77f1044d9ace4703bb738ea704434d2f2b2
Etienne Bruines <etienne.bruines@huperwebs.nl>
2015-11-07 22:12:49 +0100

    Menu now has items
    Unclickable / unselectable items are being rendered

--- systems/menu.go
+++ systems/menu.go
@@ -1,25 +1,40 @@
 package systems
 
 import (
-	"image/color"
-
-	"bitbucket.org/etiennebruines/bcigame/helpers"
+	"github.com/EtienneBruines/bcigame/helpers"
 	"github.com/paked/engi"
+	"image/color"
+	"log"
+	"os"
 )
 
 var (
-	MenuColorBackground = color.RGBA{255, 255, 255, 125}
-	MenuColorBox        = color.RGBA{180, 180, 180, 255}
-	menuColor1          = color.RGBA{102, 153, 0, 255}
-	menuColor2          = color.RGBA{153, 102, 0, 255}
+	MenuColorBackground          = color.RGBA{255, 255, 255, 125}
+	MenuColorBox                 = color.RGBA{180, 180, 180, 255}
+	MenuColorItemBackground      = color.RGBA{0, 0, 0, 125}
+	MenuColorItemBackgroundFocus = color.RGBA{64, 96, 0, 200}
+	MenuColorItemForeground      = engi.Color{255, 255, 255, 255}
+	MenuColorItemBox             = color.RGBA{230, 230, 230, 255}
+
+	menuItemHeight      = float32(25)
+	menuItemOffsetX     = float32(menuPadding + menuItemPadding)
+	menuItemFontPadding = float32(1)
+	menuItemPadding     = float32(2.5)
 	menuPadding         = float32(50)
 )
 
+type MenuItem struct {
+	Text     string
+	Callback func()
+}
+
 type Menu struct {
 	*engi.System
-	coreID string
 
-	menuActive bool
+	menuActive   bool
+	menuEntities []*engi.Entity
+	menuFocus    int
+	items        []MenuItem
 }
 
 func (*Menu) Type() string {
@@ -32,11 +47,24 @@ func (m *Menu) New() {
 	e := engi.NewEntity([]string{m.Type()})
 	e.AddComponent(&engi.UnpauseComponent{})
 	m.AddEntity(e)
-	m.coreID = e.ID()
+	m.items = []MenuItem{
+		{"New Game", func() {
+			log.Println("New game")
+			m.closeMenu()
+		}},
+		{"Calibrate", func() {
+			log.Println("Calibrate")
+			m.closeMenu()
+		}},
+		{"Exit", func() {
+			os.Exit(0)
+		}},
+	}
 }
 
 func (m *Menu) Update(entity *engi.Entity, dt float32) {
-	if engi.Keys.KEY_ESCAPE.JustPressed() {
+	// Check for ESCAPE
+	if engi.Keys.KEY_ESCAPE.JustReleased() {
 		if m.menuActive {
 			m.closeMenu()
 		} else {
@@ -44,6 +72,15 @@ func (m *Menu) Update(entity *engi.Entity, dt float32) {
 		}
 		m.menuActive = !m.menuActive
 	}
+
+	if m.menuActive {
+
+	}
+
+	// Check if any button/item is being hovered
+
+	// Check if any button/item is clicked
+
 }
 
 func (m *Menu) closeMenu() {
@@ -51,10 +88,8 @@ func (m *Menu) closeMenu() {
 	engi.Mailbox.Dispatch(engi.PauseMessage{false})
 
 	// Remove all entities
-	for _, e := range m.Entities() {
-		if e.ID() != m.coreID {
-			m.World.RemoveEntity(e)
-		}
+	for _, e := range m.menuEntities {
+		m.World.RemoveEntity(e)
 	}
 }
 
@@ -71,9 +106,10 @@ func (m *Menu) openMenu() {
 		MenuColorBackground, MenuColorBackground,
 		backgroundWidth, backgroundHeight,
 		0, 0,
-		engi.HUDGround, m.Type(),
+		engi.HUDGround,
 	)
 	menuBackground.AddComponent(&engi.UnpauseComponent{})
+	m.menuEntities = append(m.menuEntities, menuBackground)
 	m.World.AddEntity(menuBackground)
 
 	// - box
@@ -84,9 +120,56 @@ func (m *Menu) openMenu() {
 		MenuColorBox, MenuColorBox,
 		menuWidth, menuHeight,
 		menuPadding, menuPadding,
-		engi.HUDGround+1, m.Type(),
+		engi.HUDGround+1,
 	)
 	menuEntity.AddComponent(&engi.UnpauseComponent{})
+	m.menuEntities = append(m.menuEntities, menuEntity)
 	m.World.AddEntity(menuEntity)
 
+	// - items - font
+	itemFont := (&engi.Font{URL: "Roboto-Regular.ttf", Size: 64, FG: MenuColorItemForeground})
+	if err := itemFont.CreatePreloaded(); err != nil {
+		log.Fatalln("Could not load font:", err)
+	}
+	labelFontScale := float32(18 / itemFont.Size)
+
+	// - items - entities
+	offsetY := float32(menuPadding + menuItemPadding)
+	for itemID, item := range m.items {
+		var menuColorItemBackground = MenuColorItemBackground
+		if itemID == m.menuFocus {
+			menuColorItemBackground = MenuColorItemBackgroundFocus
+		}
+
+		menuItemBackground := helpers.GenerateSquare(
+			menuColorItemBackground, menuColorItemBackground,
+			menuWidth-2*menuItemPadding, menuItemHeight,
+			menuItemOffsetX, offsetY,
+			engi.HUDGround+2,
+		)
+		menuItemBackground.AddComponent(&engi.UnpauseComponent{})
+		m.menuEntities = append(m.menuEntities, menuItemBackground)
+		m.World.AddEntity(menuItemBackground)
+
+		menuItemLabel := engi.NewEntity([]string{"RenderSystem"})
+		menuItemLabelRender := &engi.RenderComponent{
+			Display:      itemFont.Render(item.Text),
+			Scale:        engi.Point{labelFontScale, labelFontScale},
+			Label:        "",                 // TODO: unused?
+			Priority:     engi.HUDGround + 3, //
+			Transparency: 1,                  //
+			Color:        0xffffff,           // TODO: unused?
+		}
+		menuItemLabel.AddComponent(menuItemLabelRender)
+		menuItemLabel.AddComponent(&engi.SpaceComponent{
+			engi.Point{
+				menuItemOffsetX + (menuItemHeight-float32(itemFont.Size)*labelFontScale)/2,
+				offsetY + menuItemFontPadding + (menuItemHeight-float32(itemFont.Size)*labelFontScale)/2,
+			}, 20, 20}) // TODO: unused ??
+		menuItemLabel.AddComponent(&engi.UnpauseComponent{})
+		m.menuEntities = append(m.menuEntities, menuItemLabel)
+		m.World.AddEntity(menuItemLabel)
+
+		offsetY += menuItemHeight + menuItemPadding
+	}
 }
